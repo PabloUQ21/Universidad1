464591ad48cb69d3d44ee0c36bc3f30feceecc6c
PabloUQ21 <juanp.londonog@uqvirtual.edu.co>
2025-02-23 23:09:25 -0500

    Empleado.java

--- src/Empleado.java
+++ src/Empleado.java
@@ -1,4 +1,75 @@
-package PACKAGE_NAME;
+import java.time.LocalDate;
 
-public class Emplado {
+public class Empleado {
+    private String id,nombre,puesto,correo,telefono;
+    private String fechaContratacion;
+
+    public Empleado(String id, String nombre, String puesto, String correo, String telefono, String fechaContratacion) {
+        this.id = id;
+        this.nombre = nombre;
+        this.puesto = puesto;
+        this.correo = correo;
+        this.telefono = telefono;
+        this.fechaContratacion = fechaContratacion;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPuesto() {
+        return puesto;
+    }
+
+    public void setPuesto(String puesto) {
+        this.puesto = puesto;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getFechaContratacion() {
+        return fechaContratacion;
+    }
+
+    public void setFechaContratacion(String fechaContratacion) {
+        this.fechaContratacion = fechaContratacion;
+    }
+
+    @Override
+    public String toString() {
+        return "Empleado{" +
+                "id='" + id + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", puesto='" + puesto + '\'' +
+                ", correo='" + correo + '\'' +
+                ", telefono='" + telefono + '\'' +
+                ", fechaContratacion=" + fechaContratacion +
+                '}';
+    }
 }
